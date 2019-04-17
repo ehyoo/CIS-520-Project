a36debc41c19a6a7a558b8194a2d330c728ca456
Lynx1820 <dkeren@seas.upenn.edu>
2019-04-17 12:32:27 -0400

    backup code - plan to change entire pipeline

--- src/sentence_vectorizer.py
+++ src/sentence_vectorizer.py
@@ -1,54 +1,60 @@
-# Import Libraries
-from sklearn.model_selection import train_test_split
 import gzip
-import gensim 
 import re 
 import string
 import nltk
-from nltk.corpus import stopwords
+import time
+import pickle
+import math
 from nltk.tokenize import word_tokenize
 from numpy import zeros
+from pymagnitude import Magnitude
 
-# Data Processing
-# All letter to lower case 
-# Remove numbers
-# removing punctuations
-data_filename = "../tiny_sample.csv"
-raw_data = open(data_filename)
+def time_since(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
 
-# Sentence to lower case 
-stop_words = set(stopwords.words('english'))
-comments = []
-labels = []
 
-for line in raw_data: 
-    labels.append(int(line.split('\t')[0]))
-    line = line.split('\t')[1].lower()
-    line = re.sub(r'\d+', '', line) 
-    line = line.translate(str.maketrans('', '', string.punctuation))
-    tokens = word_tokenize(line)
-    result = [i for i in tokens if not i in stop_words]
-    comments.append(result)
+def preprocess(file_name, m, word2vec_magnitude_file):
+        raw_data = open(file_name)
+        X_pickle_file = open(file_name[:-4] + "_X.p",'wb') 
+        y_pickle_file = open(file_name[:-4] + "_y.p",'wb') 
+        comments = []
+        y = []
 
-max_len = len(max(comments,key=len)) #denote n
-m = 10 #number of samples
+        for line in raw_data: 
+                y.append(int(line.split('\t')[0]))
+                line = line.split('\t')[1]
+                line = re.sub(r'\d+', '', line) 
+                tokens = word_tokenize(line)
+                comments.append(tokens)
 
-# matrix m x n x d, m = sample, n = max_len of sentence, d = word vector size
-# TODO: Save model? 
-# build vocabulary and train model, TODO: change vector size to something reasonable 
-vector_size = 10
-model = gensim.models.Word2Vec(comments, size=vector_size, window=8, min_count=1, workers=10)
-model.train(comments, total_examples=len(comments), epochs=10)
+        #max_len = len(max(comments,key=len)) #denote n
+        max_len = 100
+        # matrix m x n x d, m = sample, n = max_len of sentence, d = word vector size
+        # TODO: If time allows, train on our data. For now, we just Google's pretrained model
+        vector_size = 300
+        #model = gensim.models.Word2Vec(comments, size=vector_size, window=8, min_count=1, workers=10)
+        #model.train(comments, total_examples=len(comments), epochs=10)
+        #TODO: Use FastText to handle unseen words, also Magnitude! 
+        model = Magnitude(word2vec_magnitude_file)
+        X = zeros([m,max_len,vector_size])
+        #start = time.time()
+        for comment_idx,sentence in enumerate(comments): 
+                for sentence_idx, word in enumerate(sentence): 
+                        if(sentence_idx == max_len -1): break
+                        X[comment_idx][sentence_idx] = model.query(word)
+                #if(comment_idx % 250 == 0):
+                #       print(comment_idx)
+                #      print(time_since(start))
+        pickle.dump(X,X_pickle_file)
+        pickle.dump(y,y_pickle_file)
+        X_pickle_file.close()
+        y_pickle_file.close()
 
-X = zeros([m,max_len,vector_size])
-for comment_idx,sentence in enumerate(comments): 
-    for sentence_idx, word in enumerate(sentence): 
-        X[comment_idx][sentence_idx] = model.wv[word]
-
-# train test split. Size of train data is 80% and size of test data is 20%. 
-features_train, features_test, targets_train, targets_test = train_test_split(X, labels, test_size = 0.2, random_state = 42) 
-
-#print(features_test)
-#print(features_train)
-#print(targets_test)
-#print(targets_train)
+num_samples = 764173 #lines in test 
+filename = "./pol_test_cleaned.csv"
+word2vec_magnitude_file = "./GoogleNews-vectors-negative300.magnitude"
+preprocess(filename, num_samples, word2vec_magnitude_file)
